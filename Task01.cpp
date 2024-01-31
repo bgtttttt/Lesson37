816376149b7b9d5d7fe5b3bb64b8da15cc9acb9e
bgtttttt <104512912+bgtttttt@users.noreply.github.com>
2024-01-31 17:43:52 +0300

    Update Task01.cpp

--- Task01.cpp
+++ Task01.cpp
@@ -4,10 +4,6 @@ using namespace std;
 
 int main() {
 	cout << "\n......\n";
-	cout << "\n......\n";
-	cout << "\n......\n";
-	cout << "\n......\n";
-	cout << "\n......\n";
-	cout << "\n......\n";
+
 	return 0;
 }
